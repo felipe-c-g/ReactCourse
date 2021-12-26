12e4acdd2622bd263e67d0887d1a4d19fe43ecdd
felipe-c-g <f167176@dac.unicamp.br>
2021-12-26 00:46:37 -0300

    return to add new after submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -59,6 +59,7 @@ const ExpenseForm = (props) => {
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
+        props.onCancel();
     };
 
 
@@ -86,7 +87,7 @@ const ExpenseForm = (props) => {
                     <input 
                         type="date" 
                         value={enteredDate} 
-                        min="2021-01-01" 
+                        min="2019-01-01" 
                         max="2022-01-01" 
                         onChange={dateChangeHandler}/>
                 </div>
